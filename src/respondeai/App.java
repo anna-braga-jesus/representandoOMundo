39c4ff46ff0fc78424275d8c1d44e465367d7718
anna-braga-jesus <annajesus.nave@gmail.com>
2023-04-09 14:38:20 -0300

    feat: add encapsulation

--- src/respondeai/App.java
+++ src/respondeai/App.java
@@ -6,17 +6,24 @@ public class App {
     User user = new User();
 
     user.name = "Anna";
-    Adress userAdress = new Adress();
-    userAdress.city = "Rio de Janeiro";
-    userAdress.district = "Centro";
-    userAdress.state = "Rio de Janeiro";
-    userAdress.number = 30;
-    userAdress.street = "Rua Brasília";
+    user.adress.city = "Rio de Janeiro";
+    user.adress.district = "Centro";
+    user.adress.state = "Rio de Janeiro";
+    user.adress.number = 30;
+    user.adress.street = "Rua Brasília";
     user.setEmail("annajesus.nave@gmail.com");
+    user.setPassword("novasenha123");
+    User carol = new User();
+    carol.name = "Carolina Braga";
 
-    user.adress = userAdress;
+    carol.adress.city = "Unaí";
+
     System.out.println(user.adress.city);
+
     System.out.println(user.getEmail());
+    System.out.println(user.getPassword());
+    System.out.println(user.name + ": " + user.adress.city);
+    System.out.println(carol.name + ": " + carol.adress.city);
 
     }
 }
--- src/respondeai/User.java
+++ src/respondeai/User.java
@@ -3,15 +3,21 @@ package respondeai;
 public class User {
     int age;
     String name;
-    Adress adress;
-    String email;
-    String password;
+    Adress adress = new Adress();
+    private String email;
+    private String password;
 
     public String getEmail() {
 		return this.email;
 	}
+    public String getPassword() {
+		return this.password;
+	}
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+    public void setPassword(String password) {
+		this.password = password;
 	}
 }
